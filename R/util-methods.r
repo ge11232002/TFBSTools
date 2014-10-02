86fb4b399ac1774c5c0be9f23a740edcdde3cd43
Ge Tan <ge.tan09@imperial.ac.uk>
2014-10-02 12:04:51 +0000

    version 1.3.10. Add shannon.entropy
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/TFBSTools@94847 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/util-methods.r
+++ R/util-methods.r
@@ -35,3 +35,16 @@ findLargestOverlaps = function(query, subject){
   return(hits[unname(unlist(splittedLength))])
 }
 
+### -----------------------------------------------------------------
+### shannon.entropy
+### Exported!
+shannon.entropy <- function(p)
+{
+  if (min(p) < 0 || sum(p) <= 0)
+    return(NA)
+  p.norm <- p[p>0]/sum(p)
+  -sum(log2(p.norm)*p.norm)
+}
+
+
+
