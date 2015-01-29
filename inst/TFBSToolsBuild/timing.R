a6141af2691fffed0b141116eeb73605bb826c54
Ge Tan <gtan@me.com>
2015-01-29 20:14:09 +0000

    update

--- inst/TFBSToolsBuild/timing.R
+++ inst/TFBSToolsBuild/timing.R
@@ -5,10 +5,12 @@ library(CNEr)
 pfm <- getMatrixByID(JASPAR2014, ID="MA0139")
 pwm <- toPWM(pfm)
 
+### Timing for Axt alignments
 axtFns <- list.files("/export/downloads/ucsc/axtNet/hg19",
                      pattern=".*hg19.mm10.net.axt.gz",
                      full.names=TRUE)
 axt <- readAxt(axtFns)
+axt <- axt[1:2000] ## 1124711bp
 
 cputime <- proc.time()
 sitePairSet <-  searchAln(pwm, axt, min.score="80%",
