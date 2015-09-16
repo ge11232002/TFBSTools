8bb9aec7fdabfc6aaa460a7114180009a803d7de
Ge Tan <gtan@me.com>
2015-09-16 16:51:19 +0100

    1.7.5. unitTest passes

--- inst/unitTests/test_TFFM.R
+++ inst/unitTests/test_TFFM.R
@@ -3,25 +3,23 @@ test_readXMLTFFM <- function(){
   # TFFMFirst
   xmlFirst <- file.path(system.file("extdata", package="TFBSTools"),
                         "tffm_first_order.xml")
-  ## just check this does throw an error
-  checkException(tffmFirst <- readXMLTFFM(xmlFirst, type="First"))
+  tffmFirst <- readXMLTFFM(xmlFirst, type="First")
   ## check the length of emission distribution parameters
   checkIdentical(length(tffmFirst@emission), 17L)
   ## check the dimensions of transition probabilities.
-  checkIdentical(dim(tffmFirst@transition, c(17L, 16L)))
+  checkIdentical(dim(tffmFirst@transition), c(17L, 16L))
   ## check the information content of TFFMFirst
-  checkIdentical(sum(totalIC((tffmFirst))), 8.744361)
+  checkEquals(sum(totalIC((tffmFirst))), 8.744361, tolerance=1e-5)
   
   # TFFMDetail
   xmlDetail <- file.path(system.file("extdata", package="TFBSTools"),
                          "tffm_detailed.xml")
-  ## just check this does throw an error 
-  checkException(tffmDetail <- readXMLTFFM(xmlDetail, type="Detail"))
+  tffmDetail <- readXMLTFFM(xmlDetail, type="Detail")
   ## check the length of emission distribution parameters
   checkIdentical(length(tffmDetail@emission), 64L)
   ## check the dimensions of transition probabilities.
-  checkIdentical(dim(tffmDetail@transition, c(64L, 64L)))
+  checkIdentical(dim(tffmDetail@transition), c(64L, 64L))
   ## check the information content of TFFMDetail
-  checkIdentical(sum(totalIC((tffmDetail))), 7.745002)
+  checkEquals(sum(totalIC((tffmDetail))), 7.745002, tolerance=1e-5)
 }
 
