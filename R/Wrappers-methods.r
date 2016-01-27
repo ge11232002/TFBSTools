f08c1575fcd203acd02d399a5392b1660f0c08e1
Ge Tan <gtan@me.com>
2016-01-27 20:39:53 +0000

    version 1.9.10.
    Fix runMEME for meme 4.10.* version. Add citation

--- R/Wrappers-methods.r
+++ R/Wrappers-methods.r
@@ -3,32 +3,31 @@
 ### ---------------------------------------------------------------
 ### The real wrapper function for MEME
 ###
-# inputFastaFn = "/Users/gtan/src/meme_4.9.1/tests/crp0.s"
-run_MEME = function(inputFastaFn, binary="meme", seqtype="DNA", 
+run_MEME <- function(inputFastaFn, binary="meme", seqtype="DNA", 
                     arguments=list()){
-  valueArguments = c("-mod", "-nmotifs", "-evt", "-nsites", 
+  valueArguments <- c("-mod", "-nmotifs", "-evt", "-nsites", 
                         "-minsites", "-maxsites", "-wnsites",
                         "-w", "-minw", "-maxw", "-wg", "-ws",
                         "-bfile", "-maxiter", "-distance",
                         "-psp", "-prior", "-b", "-plib",
                         "-spfuzz", "-spmap", "-cons", "-heapsize",
                         "-maxsize", "-p", "-time", "-sf")
-  booleanArguments = c("-nomatrim", "-noendgaps", "-revcomp", "-pal",
+  booleanArguments <- c("-nomatrim", "-noendgaps", "-revcomp", "-pal",
                        "-x_branch", "-w_branch")
-  arguments1 = arguments[names(arguments) %in% valueArguments]
-  arguments1 = paste(names(arguments), arguments, collapse=" ")
-  arguments2 = arguments[names(arguments) %in% booleanArguments]
-  arguments2 = paste(names(arguments2), collapse=" ")
-  arguments = paste(arguments1, arguments2)
-  cmd = paste(binary, inputFastaFn, "-text", 
+  arguments1 <- arguments[names(arguments) %in% valueArguments]
+  arguments1 <- paste(names(arguments), arguments, collapse=" ")
+  arguments2 <- arguments[names(arguments) %in% booleanArguments]
+  arguments2 <- paste(names(arguments2), collapse=" ")
+  arguments <- paste(arguments1, arguments2)
+  cmd <- paste(binary, inputFastaFn, "-text", 
               ifelse(seqtype=="DNA", "-dna", "-protein"), 
               arguments, "2>/dev/null")
-  memeOutput = my.system(cmd, intern=TRUE)
+  memeOutput <- my.system(cmd, intern=TRUE)
   #conMemeOutput = pipe(cmd, open="rt")
   #on.exit(close(conMemeOutput))
   
   # get the version of meme used.
-  oneLine = memeOutput[grep("^MEME version", memeOutput)]
+  oneLine <- memeOutput[grep("^MEME version", memeOutput)]
   version = strsplit(oneLine, " ")[[1]][3]
 
   # get the command
@@ -40,9 +39,9 @@ run_MEME = function(inputFastaFn, binary="meme", seqtype="DNA",
   revcomp = grepl("revcomp", command)
   oneLines = memeOutput[grep("^MOTIF  \\d+", memeOutput)]
   splittedLines = strsplit(oneLines, "[[:blank:]]+")
-  motifWidths = as.integer(sapply(splittedLines, "[", 5))
-  motifOccurrences = as.integer(sapply(splittedLines, "[", 8))
-  motifEvalues = as.numeric(sapply(splittedLines, "[", 14))
+  motifWidths = as.integer(sapply(splittedLines, "[", 6))
+  motifOccurrences = as.integer(sapply(splittedLines, "[", 9))
+  motifEvalues = as.numeric(sapply(splittedLines, "[", 15))
   
   # get the motifs names
   indexNames = grep("sorted by position p-value", memeOutput)
@@ -56,15 +55,19 @@ run_MEME = function(inputFastaFn, binary="meme", seqtype="DNA",
     oneLines = memeOutput[seq(from=indexNames[i]+4, 
                               to=indexNames[i]+4+motifOccurrences[i]-1)]
     splittedLines = strsplit(oneLines, "[[:blank:]]+")
-    oneRange = GRanges(seqnames=sapply(splittedLines, "[", 1), 
-                       ranges=IRanges(start=
-                                      as.integer(sapply(splittedLines, 
-                                                        "[", 
-                                                        ifelse(revcomp, 3, 2))), 
+    strands <- NULL
+    starts <- NULL
+    if(revcomp){
+      strands <- sapply(splittedLines, "[", 2)
+      starts <- as.integer(sapply(splittedLines, "[", 3))
+    }else{
+      strands <- Rle("+", length(splittedLines))
+      starts <- as.integer(sapply(splittedLines, "[", 2))
+    }
+    oneRange <- GRanges(seqnames=sapply(splittedLines, "[", 1), 
+                       ranges=IRanges(start=starts,
                                       width=motifWidths[i]),
-                       strand=ifelse(revcomp, 
-                                     list(sapply(splittedLines, "[", 2)), 
-                                     list("+"))[[1]],
+                       strand=strands,
                        score=as.numeric(sapply(splittedLines, 
                                                "[", ifelse(revcomp, 4, 3)))
                        )
@@ -89,7 +92,7 @@ setMethod("runMEME", "character",
                            arguments=arguments)
             subjectSeqs = switch(seqtype,
                                  "DNA"=readDNAStringSet(filepath=x, 
-                                                        format="fasta",),
+                                                        format="fasta"),
                                  "AA"=readAAStringSet(filepath=x, 
                                                       format="fasta")
                                  )
