f14bf09bdff9c9f67cc2fa661f096eec0369b6f3
Ge Tan <gtan@me.com>
2016-11-24 12:02:56 +0100

    fix the test

--- tests/testthat/test_MEME.R
+++ tests/testthat/test_MEME.R
@@ -3,6 +3,6 @@ test_that("parseMEMEOutput", {
                           "meme.output")
   ans <- parseMEMEOutput(memeOutput)
   expect_equal(length(ans$motifList), 5L)
-  expect_equal(lengths(ans$motifList), c(17L, 5L, 2L, 5L, 2L))
+  expect_equivalent(lengths(ans$motifList), c(17L, 5L, 2L, 5L, 2L))
   expect_equal(ans$motifEvalues, c(4.1e-09, 2.8e+02, 1.2e+03, 1.4e+03, 1.8e+03))
 })
